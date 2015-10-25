bdf4529258dc2b4547498fd442bc1e9530f16a58
Hugo <sanchezhugo.1994@gmail.com>
2015-10-24 22:58:16 -0400

    -a

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+//App that prints "Hello World".
+//Will be used to test applet in html
+
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("Hello World");
+	}
+}
